2a9ce9fb6dd94bac96838b1abdea40aba98f3acc
ruben-suarez-dev <ruben.suarezpro.dev@gmail.com>
2024-04-12 11:21:00 -0400

    Portafolio finalizado parte 1

--- portafolio-ruben/tailwind.config.mjs
+++ portafolio-ruben/tailwind.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
 	theme: {
 		extend: {
 			colors: {
-				'hoverInfoButton': '#12538F'
+				'hoverInfoButton': '#12538F',
+				'blue-special': '#0A2E4F',
+				'blue-experience': '#0F1520',
+				'black-border': '#24272d',
+				'yellow-subtitle': '#F9FEA4',
+				'black-card': '#1E1E20'
 			}
 		}
 	},
